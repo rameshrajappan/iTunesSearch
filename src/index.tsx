6b517a0ddfe3ecf3af366ddf9eb0e8b0cca44308
rameshrajappan <rameshrajappan@gmail.com>
2023-03-09 01:06:56 +0530

    unit testing initial changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import store from './store';
+import store from './app/store';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
